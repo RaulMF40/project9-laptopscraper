ddf37385f81042ec66be2978a7bffccc4f695ae0
RaulMF40 <144492714+RaulMF40@users.noreply.github.com>
2024-07-10 13:19:23 +0200

    Update db.js

--- src/config/db.js
+++ src/config/db.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose')
 const connectDB = async () => {
   try {
     const DB_URL =
-      'mongodb+srv://webscrapping:z55xi6wvC2YdjD1O@cluster0.nzfy3zs.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0'
+      'mongodb+srv://webscrapping:vDCcrhA2Zb8aH8t@cluster0.nzfy3zs.mongodb.net/?appName=Cluster0'
     await mongoose.connect(DB_URL)
     console.log('Connected to DB')
   } catch (error) {
@@ -11,4 +11,4 @@ const connectDB = async () => {
   }
 }
 
-module.exports = connectDB
+module.exports = { connectDB }
