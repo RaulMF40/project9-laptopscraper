869417fb6eac13b06caf2dbaf22fbc5fb03b84ae
RaulMF40 <144492714+RaulMF40@users.noreply.github.com>
2024-07-10 13:20:20 +0200

    Update laptop.js

--- src/api/controllers/laptop.js
+++ src/api/controllers/laptop.js
@@ -1,14 +1,14 @@
 const Laptop = require('../models/laptop')
 
 // Crear un nuevo producto
-//!------------------
 const addLaptop = async (req, res, next) => {
   try {
-    const newLaptop = new Laptop(req.body)
     const existingLaptop = await Laptop.findOne({ title: req.body.title })
     if (existingLaptop) {
       return res.status(400).json('Este portatil ya está registrado')
     }
+
+    const newLaptop = new Laptop(req.body)
     const savedLaptop = await newLaptop.save()
     return res.status(201).json(savedLaptop)
   } catch (error) {
@@ -16,59 +16,66 @@ const addLaptop = async (req, res, next) => {
     return res.status(400).json(error)
   }
 }
-//!---------------
 
 // Actualizar un producto por ID
-//!-------------------
 const editLaptop = async (req, res, next) => {
   try {
     const { id } = req.params
-    const newLaptop = new Laptop(req.body)
-    newLaptop._id = id
-    const updatedLaptop = await Laptop.findByIdAndUpdate(id, newLaptop, {
+    const updatedLaptop = await Laptop.findByIdAndUpdate(id, req.body, {
       new: true
     })
+
+    if (!updatedLaptop) {
+      return res.status(404).json('Portatil no encontrado')
+    }
+
     return res.status(200).json({
       message: 'Portatil actualizado correctamente',
-      juego: updatedLaptop
+      laptop: updatedLaptop
     })
   } catch (error) {
+    console.log(error)
     return res.status(400).json(error)
   }
 }
-//!----------------------
+
 // Eliminar un producto por ID
-//!--------------------
 const deleteLaptop = async (req, res, next) => {
   try {
     const { id } = req.params
     const deletedLaptop = await Laptop.findByIdAndDelete(id)
-    if (deletedLaptop) {
-      return res
-        .status(200)
-        .json({ mensaje: 'Portatil eliminado', laptop: deletedLaptop })
-    } else {
+
+    if (!deletedLaptop) {
       return res.status(404).json('Portatil no encontrado')
     }
+
+    return res.status(200).json({
+      mensaje: 'Portatil eliminado',
+      laptop: deletedLaptop
+    })
   } catch (error) {
+    console.log(error)
     return res.status(400).json(error)
   }
 }
-//!---------------------
-const getLaptop = async (req, res, next) => {
+
+// Obtener todos los laptops
+const getLaptops = async (req, res, next) => {
   try {
-    const allLaptop = await Laptop.find().populate('laptop')
-    if (allLaptop.length === 0) {
+    const allLaptops = await Laptop.find()
+    if (allLaptops.length === 0) {
       return res
         .status(204)
         .json('Todavía no hay portatiles cargados en la base de datos')
     }
-    return res.status(200).json(allLaptop)
+    return res.status(200).json(allLaptops)
   } catch (error) {
-    return res
-      .status(400)
-      .json({ mensaje: 'Error buscando los portatiles', error: error })
+    console.log(error)
+    return res.status(400).json({
+      mensaje: 'Error buscando los portatiles',
+      error: error
+    })
   }
 }
-//!-----------
-module.exports = { addLaptop, editLaptop, deleteLaptop, getLaptop }
+
+module.exports = { addLaptop, editLaptop, deleteLaptop, getLaptops }
